918037cc42a02fc3162d0de3338babc51592f9d1
nospame <mapson.evan@gmail.com>
2022-03-28 12:34:44 -0700

    add recipes index route with encrypted 3rd-party api key

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,9 @@
 class RecipesController < ApplicationController
   def index
-    render json: {message: "Recipes Index"}
+    app_id = Rails.application.credentials.recipes_app_id
+    app_key = Rails.application.credentials.recipes_api_key
+    response = HTTP.get("https://api.edamam.com/api/recipes/v2?type=public&q=breakfast&app_id=#{app_id}&app_key=#{app_key}")
+    recipes = response.parse(:json)
+    render json: recipes.as_json
   end
 end
